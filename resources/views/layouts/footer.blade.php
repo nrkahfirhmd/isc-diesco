2575daf07cb30e53cc051ae1bde447b4fb6d5f74
Nurkahfi A Rahmada <110613182+nrkahfirhmd@users.noreply.github.com>
2024-08-06 16:00:03 +0700

    add footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,44 @@
+@include('layouts.header')
+
+<head>
+    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
+</head>
+<body class="viewport">
+    <footer class="footer">
+        <div class="wrap">
+            <div class="info-wrapper">
+                <div class="logo">
+                    <img src="{{ asset('images/dekorin.png') }}" alt="dekorin">
+                </div>
+                <div class="address">
+                    <p>Jl.Raya Kalimulya, RT01/05</p>
+                    <p>Kel. Kalimulya Kec. Cilodong, Depok, Jawa Barat</p>
+                </div>
+            </div>
+            <div class="copyright">
+                <p>&copy; 2024 by PT Dekorin Sinarmas. All right reserved</p>
+            </div>
+        </div>
+        <div class="center">
+            <div class="info">
+                <p>Pricing</p>
+                <p>Features</p>
+            </div>
+            <div class="info">
+                <p>Categories</p>
+                <p>Join Us</p>
+            </div>
+        </div>
+        <div class="wrap">
+            <div class="socmed">
+                <img src="{{ asset('images/Instagram.png') }}" alt="instagram">
+                <img src="{{ asset('images/Twitter.png') }}" alt="twitter">
+                <img src="{{ asset('images/Facebook.png') }}" alt="facebook">
+            </div>
+            <div class="tnc">
+                <p>Terms and Conditions</p>
+                <p>Privacy Policy</p>
+            </div>
+        </div>
+    </footer>
+</body>
