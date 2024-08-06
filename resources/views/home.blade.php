737ae2086aca86efe5cc70b089161bb7f36b50bd
rumirifai <rumirifai96@gmail.com>
2024-08-06 17:27:07 +0700

    landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,11 @@
 <head>
     <link rel="stylesheet" href=" {{ asset('css/home.css') }} ">
 </head>
+
 <body>
     <nav class="navbar">
         <a href="" class="logo">
-            <img src="/img/home-logo.png" alt="Dekorin">
+            <img src="/images/dekorin.png" alt="Dekorin">
         </a>
         <div class="nav-items">
             <div class="links">
@@ -18,4 +19,49 @@
             <button class="sign-up">Masuk</button>
         </div>
     </nav>
+
+    <div class="hero">
+        
+    </div>
+
+    <div class="card-list">
+        <div class="card">
+            <img src="{{ asset('images/cardlist/floor-lamp.png') }}" alt="floor lamp">
+            <div class="card-content">
+                <h3>Desain Interior</h3>
+            </div>
+        </div>
+        <div class="card">
+            <img src="{{ asset('images/cardlist/shower.png') }}" alt="shower">
+            <div class="card-content">
+                <h3>Kamar Mandi</h3>
+            </div>
+        </div>
+        <div class="card">
+            <img src="{{ asset('images/cardlist/sofa.png') }}" alt="sofa">
+            <div class="card-content">
+                <h3>Ruang Keluarga</h3>
+            </div>
+        </div>
+        <div class="card">
+            <img src="{{ asset('images/cardlist/grass.png') }}" alt="grass">
+            <div class="card-content">
+                <h3>Taman</h3>
+            </div>
+        </div>
+        <div class="card">
+            <img src="{{ asset('images/cardlist/microwave.png') }}" alt="microwave">
+            <div class="card-content">
+                <h3>Kitchen Set</h3>
+            </div>
+        </div>
+        <div class="card">
+            <img src="{{ asset('images/cardlist/gite.png') }}" alt="gite">
+            <div class="card-content">
+                <h3>Spesialis Atap</h3>
+            </div>
+        </div>
+    </div>
 </body>
+
+@include('layouts.footer')
