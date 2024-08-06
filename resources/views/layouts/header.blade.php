7dfe3081de2a039a0160201274346c502701e7e8
Nurkahfi A Rahmada <110613182+nrkahfirhmd@users.noreply.github.com>
2024-08-06 15:25:10 +0700

    fix bug css

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,6 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dekorin</title>
-    <link rel="stylesheet" href="app.css">
+    <link rel="stylesheet" href=" {{ asset('css/app.css') }} ">
     <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
 </head>
